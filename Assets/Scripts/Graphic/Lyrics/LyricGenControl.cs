27eb465f92e406139cab4238343bd2af9b4ff9e5
gotojo <tojotakeshizzz@gmail.com>
2025-06-09 08:10:28 +0900

    縦書き歌詞表示に対応する

--- Assets/Scripts/Graphic/Lyrics/LyricGenControl.cs
+++ Assets/Scripts/Graphic/Lyrics/LyricGenControl.cs
@@ -2,6 +2,7 @@
 // 小節毎の歌詞を取得、SMFPlayerからのイベントで適切なタイミングを判断、歌詞の切り替えを行う
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 class LyricGenControl {
 	public string curWord = "";
@@ -34,7 +35,22 @@ class LyricGenControl {
 		midiWatcher.onMeasureIn -= MeasureIn;
 		midiWatcher.onEventIn -= EventIn;
 	}
+	public string ToVertical(string input) {
+		var builder = new System.Text.StringBuilder();
 
+		foreach (char c in input) {
+			if (c == 'ー' || c == '～' || c == '―' || c == '…') {
+				builder.Append("<rotate=90>");
+				builder.Append(c);
+				builder.Append("</rotate>");
+			} else {
+				builder.Append(c);
+			}
+			builder.Append('\n');
+		}
+
+		return builder.ToString();
+	}
 	public GameObject CreateText(string word, TMP_FontAsset font, Color color, TextAlignmentOptions align, Vector2 sizeDelta, Vector3 position, float scale, float rotate) {
 		GameObject simpleLyric = new GameObject("SimpleLyric");
 		simpleLyric.AddComponent<TextMeshPro>();
@@ -47,9 +63,11 @@ class LyricGenControl {
 		text.fontSizeMin = 12;
 		text.autoSizeTextContainer = false;
 		text.alignment = align;
+		text.lineSpacing = -30;
 		Transform transform = text.GetComponent<Transform>();
 		RectTransform rectTransform = simpleLyric.GetComponent<RectTransform>();
 		rectTransform.sizeDelta = sizeDelta;
+		// rectTransform.pivot = new Vector2(0.5f, 1);
 		transform.position = position;
 		transform.Rotate(0.0f, 0.0f, rotate);
 		simpleLyric.transform.localScale = new Vector3(scale, scale, scale);
--- Assets/Scripts/Graphic/Lyrics/LyricGenMultiLine.cs
+++ Assets/Scripts/Graphic/Lyrics/LyricGenMultiLine.cs
@@ -5,27 +5,34 @@
 using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
 
 public class LyricGenMultiLine : MonoBehaviour {
 	public Rect area = new Rect(-6, -4, 20, 6);
 	public TMP_FontAsset font;
 	public float scale = 0.7f;
-	public float textHight = -1.0f;
+	public float textHight = 1.0f;
+	public float textWidth = 1f;
+
+	public bool vertical = false;
 	public int maxLine = 5;
 	public SentenceList sentenceList;
 	public bool active = true;
 	class LyricGenMultiLineControl : LyricGenControl {
 		public int maxLine = 5;
 		public float scale = 1f;
+		public bool vertical = false;
 		private List<GameObject> lyrics = new List<GameObject>();
 		private TMP_FontAsset font;
 		private LyricGenMultiLine lyricGen;
 		private Rect area;
-		private float textHeight = 2f;
+		private float textHight = 2f;
+		private float textWidth = 2f;
 		private int lyricCount = 0;
-		public LyricGenMultiLineControl(Rect area, float textHeight, TMP_FontAsset font, LyricGenMultiLine lyricGen) : base(lyricGen.sentenceList, MidiWatcher.Instance) {
+		public LyricGenMultiLineControl(Rect area, float textHight, float textWidth, TMP_FontAsset font, LyricGenMultiLine lyricGen) : base(lyricGen.sentenceList, MidiWatcher.Instance) {
 			this.area = area;
-			this.textHeight = textHeight;
+			this.textHight = textHight;
+			this.textWidth = textWidth;
 			this.font = font;
 			this.lyricGen = lyricGen;
 		}
@@ -33,10 +40,28 @@ public class LyricGenMultiLine : MonoBehaviour {
 			if (!active) return;
 			Color color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
 			float rotate = 0;
-			Vector2 size = new Vector2(area.width, textHeight);
-			float y = area.yMax - textHeight * lyricCount;
-			Vector3 position = new Vector3(area.x, y, 0);
-			GameObject lyric = CreateText(text, font, color, TextAlignmentOptions.Left, size, position, scale, rotate);
+			float x;
+			float y;
+			float w;
+			float h;
+			TextAlignmentOptions alignment;
+			if (vertical) {
+				w = textWidth;
+				h = area.height;
+				x = area.xMin - textWidth * lyricCount;
+				y = area.yMax;
+				alignment = TextAlignmentOptions.Top;
+				text = ToVertical(text);
+			} else {
+				w = area.width;
+				h = textHight;
+				x = area.x;
+				y = area.yMax - h * lyricCount;
+				alignment = TextAlignmentOptions.TopLeft;
+			}
+			Vector2 size = new Vector2(w, h);
+			Vector3 position = new Vector3(x, y, 0);
+			GameObject lyric = CreateText(text, font, color, alignment, size, position, scale, rotate);
 			lyric.transform.parent = lyricGen.transform;
 			lyrics.Add(lyric);
 			lyricCount++;
@@ -59,9 +84,10 @@ public class LyricGenMultiLine : MonoBehaviour {
 	LyricGenMultiLineControl control;
 
 	void Start() {
-		control = new LyricGenMultiLineControl(area, textHight, font, this);
+		control = new LyricGenMultiLineControl(area, textHight, textWidth, font, this);
 		control.maxLine = maxLine;
 		control.scale = scale;
+		control.vertical = vertical;
 		control.active = active;
 	}
 
