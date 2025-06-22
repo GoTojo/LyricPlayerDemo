956e0d0ed357fbcf36520d311b9543d1330ff911
gotojo <tojotakeshizzz@gmail.com>
2025-06-22 09:53:44 +0900

    RamenFloorの回転を2小節で回って2小節止める、にする

--- Assets/Scripts/Graphic/Ramen/RotateContentsZ.cs
+++ Assets/Scripts/Graphic/Ramen/RotateContentsZ.cs
@@ -1,24 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateContentsZ : MonoBehaviour {
+	public enum Type {
+		Normal,
+		HalfAndStop
+	}
+	public Type type = Type.Normal;
 	private float rotationTime = 8; // 4measure at BPM120
 									// Start is called before the first frame update
 	private int manualCount = 0;
+	private int curMeas = 0;
 	void Start() {
 		MidiWatcher midiWatcher = MidiWatcher.Instance;
 		midiWatcher.onMeasureIn += MeasureIn;
+		Vector3 angle = this.transform.eulerAngles;
+		angle.z = 0;
+		this.transform.eulerAngles = angle;
 	}
 
 	// Update is called once per frame
 	void Update() {
-		float deltaAngle = 360 * Time.deltaTime / rotationTime;
-		this.transform.Rotate(0f, 0f, -deltaAngle);
+		if (type == Type.Normal) {
+			float deltaAngle = 360 * Time.deltaTime / rotationTime;
+			this.transform.Rotate(0f, 0f, -deltaAngle);
+		} else if (type == Type.HalfAndStop) {
+			int phase = curMeas % 4;
+			if (phase < 2) {
+				float deltaAngle = 360 * Time.deltaTime / (rotationTime / 2);
+				this.transform.Rotate(0f, 0f, -deltaAngle);
+			} else {
+				Vector3 angle = this.transform.eulerAngles;
+				angle.z = 0;
+				this.transform.eulerAngles = angle;
+			}
+		}
 	}
-
 	public void MeasureIn(int measure, int measureInterval, uint currentMsec) {
-		// 4小節
+		// 4小節で一周
+		curMeas = measure;
 		if (manualCount > 0) {
 			manualCount--;
 		} else {
