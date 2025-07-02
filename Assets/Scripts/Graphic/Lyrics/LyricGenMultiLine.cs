ae2801ffd18a6977d8a45597a342d1434565e39e
gotojo <MXI03066@gmail.com>
2025-07-03 07:00:35 +0900

    LyricGenMultiLineで1文字ずつ表示できるようにする準備のリファクタリングをする

--- Assets/Scripts/Graphic/Lyrics/LyricGenMultiLine.cs
+++ Assets/Scripts/Graphic/Lyrics/LyricGenMultiLine.cs
@@ -41,33 +41,41 @@ public class LyricGenMultiLine : MonoBehaviour {
 			this.font = font;
 			this.lyricGen = lyricGen;
 		}
-		private void CreateText(string text) {
-			if (!active) return;
-			Color color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
-			float rotate = 0;
+		private void GetTextArea(string text, ref Vector3 position, ref Vector2 size) {
 			float x;
 			float y;
 			float w;
 			float h;
-			TextAlignmentOptions alignment;
 			if (vertical) {
 				w = textWidth;
 				h = area.height;
 				x = area.xMin - textWidth * lyricCount;
 				y = area.yMax;
-				alignment = TextAlignmentOptions.Top;
-				text = ToVertical(text);
 			} else {
 				w = area.width;
 				h = textHeight;
 				x = area.x;
 				y = area.yMax - h * lyricCount;
+			}
+			size = new Vector2(w, h);
+			position = new Vector3(x, y, 0);
+		}
+		private void CreateText(string text) {
+			if (!active) return;
+			Color color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
+			float rotate = 0;
+			TextAlignmentOptions alignment;
+			Vector2 size = new Vector2();
+			Vector3 position = new Vector3();
+			GetTextArea(text, ref position, ref size);
+			if (vertical) {
+				alignment = TextAlignmentOptions.Top;
+				text = ToVertical(text);
+			} else {
 				alignment = TextAlignmentOptions.TopLeft;
 			}
-			Vector2 size = new Vector2(w, h);
-			Vector3 position = new Vector3(x, y, 0);
 			GameObject lyric = CreateText(text, font, color, alignment, size, position, scale, rotate);
-			lyric.transform.parent = lyricGen.transform;
+			lyric.transform.SetParent(lyricGen.transform);
 			lyrics.Add(lyric);
 			lyricCount++;
 			waitClear = waitCount;
