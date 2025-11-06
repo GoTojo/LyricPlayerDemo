ec399ed0c3c9c0d831d914c98a872e6e5712f221
gotojo <MXI03066@gmail.com>
2025-11-06 23:58:52 +0900

    サイズを調整

--- Assets/Scripts/Graphic/Lyrics/SimpleLyricGen.cs
+++ Assets/Scripts/Graphic/Lyrics/SimpleLyricGen.cs
@@ -40,7 +40,7 @@ public class SimpleLyricGen : LyricBase {
 			float width = (sentenceLength != 0) ? area.width / sentenceLength : area.width;
 			float x = width * lyricNum + area.x + width / 2;
 			float y = UnityEngine.Random.Range(area.yMin, area.yMax);
-			Vector3 pos = new Vector3(x, y, -1);
+			Vector3 pos = new Vector3(x, y, -5);
 			float scale = UnityEngine.Random.Range(sizeMin, sizeMax);
 			float rotate = UnityEngine.Random.Range(-rotateAngle, rotateAngle);
 			GameObject lyric = CreateText(curWord, color, TextAlignmentOptions.Top, sizeDelta, pos, scale, rotate);
